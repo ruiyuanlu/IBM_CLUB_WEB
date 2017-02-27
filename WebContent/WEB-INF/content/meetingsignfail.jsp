ea055a1a28ff34a3e23ba284393eafee27840852
bososha <wwuu122@yahoo.co.jp>
2017-02-27 20:52:16 +0800

    简单实现了扫码签到的基础功能实现

---
+++ WebContent/WEB-INF/content/meetingsignfail.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Morn Wu
+  Date: 2017/2/27
+  Time: 下午 8:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>签到失败</title>
+</head>
+<body>
+（建议该页面做成仅适应手机显示）
+签到时间已过！下次记得按时到会！
+</body>
+</html>
--- src/com/istc/action/AttendanceAction.java
+++ src/com/istc/action/AttendanceAction.java
@@ -1,11 +1,31 @@
 package com.istc.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
 
 /**
  * 部长及以上成员管理考勤信息
  */
-
+@Action(
+        value="Sign",
+        results={
+                @Result(name="success", location = "meetingsignsuccess.jsp"),
+                @Result(name="fail", location="meetingsignfail.jsp")
+        }
+)
 public class AttendanceAction extends ActionSupport{
+    static boolean signavailable=true;
+
+    @Override
+    public String execute() {
+        //这里写从数据库获取当前时候可以签到的变量
+        if (signavailable){
+            return SUCCESS;
+        }
+        else {
+            return "fail";
+        }
+    }
 
 }
